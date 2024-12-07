d118af9059b1435742051dfb038f3bc5de0c8a55
shakeer405 <shakeer.pca@gmail.com>
2024-12-07 14:55:44 +0000

    app engine

---
+++ appengine-demo/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, App Engine Standard! version Middle tier version 1'
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    print (port)
+    app.run(host='0.0.0.0', port=8080)
